fbea0b667916b10bbd228796536d414e18a4e171
Anna-Tatinyan <anna.tatinyan.21@mail.ru>
2017-09-07 13:11:21 +0400

    first commit

---
+++ greeting.js
@@ -0,0 +1,21 @@
+/*
+console.log("welcome!");
+console.log("mood" + 100);
+console.log(15/3*(2+3));
+*/
+console.log(isNaN("mood"*100))
+
+console.log("mood"==="mood")
+const InspirationTxt = "be happy";
+console.log(InspirationTxt)
+const mood = "100%"
+console.log("mood")
+console.log((true&&false)||(!!!true))
+
+const num1=56;
+const num2=63;
+const sum= num1+num2;
+console.log(sum);
+console.log(num2/num1)
+console.log("i am writing normal ban");
+console.log((true&&false)||(!!!true));
